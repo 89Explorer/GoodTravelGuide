b4cecd0d7f606cd3f0a889880bc7119aae394f3e
89Explorer <leral3@naver.com>
2024-08-31 19:55:59 +0900

    feat: 홈화면에 카테고리별 무작위로 데이터 선정하여 홈화면에 표시 - UI 화면에 표시 - 외부 API를 통해 얻어오는 데이터 함수를 공용으로 사용

---
+++ GoodTravelGuide/Model/AttractionResponse.swift
@@ -0,0 +1,43 @@
+//
+//  Attraction.swift
+//  GoodTravelGuide
+//
+//  Created by 권정근 on 8/31/24.
+//
+
+import Foundation
+
+struct Item: Codable {
+    let addr1
+}
+{
+  "response": {
+    "header": {
+      "resultCode": "0000",
+      "resultMsg": "OK"
+    },
+    "body": {
+      "items": {
+        "item": [
+          {
+            "addr1": "강원특별자치도 춘천시 서면 신매리",
+            "addr2": "68-4",
+            "areacode": "32",
+            "booktour": "0",
+            "cat1": "A02",
+            "cat2": "A0205",
+            "cat3": "A02050200",
+            "contentid": "128778",
+            "contenttypeid": "12",
+            "createdtime": "20060209090000",
+            "firstimage": "http://tong.visitkorea.or.kr/cms/resource/71/181971_image2_1.jpg",
+            "firstimage2": "http://tong.visitkorea.or.kr/cms/resource/71/181971_image3_1.jpg",
+            "cpyrhtDivCd": "Type3",
+            "mapx": "127.7130161606",
+            "mapy": "37.9167408445",
+            "mlevel": "6",
+            "modifiedtime": "20231123121517",
+            "sigungucode": "13",
+            "tel": "",
+            "title": "강원 경찰충혼탑"
+          },
---
+++ GoodTravelGuide/Networking/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  GoodTravelGuide
+//
+//  Created by 권정근 on 8/30/24.
+//
+
+import Foundation
